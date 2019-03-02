8947e730a97e8172bddd46b78a7ba9b17457f837
Bartek Borowczyk <bartek@websamuraj.pl>
2019-03-02 11:38:53 +0100

    Updates

---
+++ precache-manifest.2415c76c075e7c25520c9ced6bf4e771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc337fc7991e9c5069ec",
+    "url": "/react-app/static/css/main.2fd68384.chunk.css"
+  },
+  {
+    "revision": "dc337fc7991e9c5069ec",
+    "url": "/react-app/static/js/main.dc337fc7.chunk.js"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "17106d1efc994e6d245635a8c91397e3",
+    "url": "/react-app/index.html"
+  }
+];
